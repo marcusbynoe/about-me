b49470114f4a7ac0ed8e61ca5c10bbaf3f6873d2
Marcus Bynoe <marcusbynoe57@gmail.com>
2022-11-29 17:58:42 -0700

    added javascript to file

--- js/app.js
+++ js/app.js
@@ -1,19 +1,48 @@
 'use strict';
 
-console.log('Hello world!');
+// console.log('Hello World!');
 
-// TODO: GET USERS NAME - prompt - alert a greeting back to the user
+let userName = prompt('Hi, what is your name?');
+alert(`Welcome to my site ${userName}! I am going to ask you a series of questions that need a yes/no or y/n answer`);
 
-let userName = prompt('What is your name?');
-
-alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
-
-// TODO: prompt them with 5 yes or no (y/n) questions. And let them know if the got the answer right or wrong
-
-let qOne = prompt('My question');.toLowerCase();
-
+let qOne = prompt('Question 1: I am the only child?').toLowerCase();
 if(qOne === 'yes' || qOne === 'y'){
-  alert('You got it right!');
+  alert('Sorry, that is incorrect');
 } else if(qOne === 'no' || qOne === 'n'){
-  alert('Sorry, you got it wrong');
+  alert('Nice! You got it right!');
 }
+// console.log('User\'s first answer is ' + qOne);
+
+let qTwo = prompt('Question 2: I was born in the west coast?').toLowerCase();
+if(qTwo === 'yes' || qTwo === 'y'){
+  alert('Sorry, you are wrong');
+} else if(qTwo === 'no' || qTwo === 'n'){
+  alert('Awesome, you guessed correctly!');
+}
+// console.log('User\'s second answer is ' + qTwo);
+
+let qThree = prompt('Question 3: I served in the U.S Navy?').toLowerCase();
+if(qThree === 'yes' || qThree === 'y'){
+  alert('That is incorrect... I can barely swim unfortunately');
+} else if(qThree === 'no' || qThree === 'n'){
+  alert('Correct! I was in the Army!');
+}
+// console.log('User\'s third answer is ' + qThree);
+
+let qFour = prompt('Question 4: I like to play video games?').toLowerCase();
+if(qFour === 'yes' || qFour === 'y'){
+  alert('That is correct! I love gaming!');
+} else if(qFour === 'no' || qFour === 'n'){
+  alert('Incorrect! Video games is life.');
+}
+// console.log('User\'s fourth answer is ' + qFour);
+
+let qFive = prompt('Last question: Is EDM my favorite genre of music?').toLowerCase();
+if(qFive === 'yes' || qFive ==='y'){
+  alert('You guessed correctly!');
+} else if(qFive === 'no' || qFive === 'n'){
+  alert('Sorry that is the wrong answer');
+}
+// console.log('User\'s fifth answer is ' + qFive);
+
+alert(`Thanks for answering my questions ${userName}! Hope you learned a little more about me from the questionnaire!`);
